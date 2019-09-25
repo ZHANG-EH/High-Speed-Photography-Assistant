53f8e062217ce3b55f6ad98529b87b050e96e55e
ehzhang <ehzhang@umich.edu>
2019-09-25 00:16:22 -0400

    add arduino codes

---
+++ codes/flash_light_controller.ino
@@ -0,0 +1,1 @@
+void setup() {  pinMode(8,OUTPUT);  pinMode(9,OUTPUT);  digitalWrite(8,HIGH);  digitalWrite(8,LOW);  delay(5000);}void loop() {  digitalWrite(8,LOW);  delay(20);  digitalWrite(8,HIGH);}
---
+++ codes/shutter_controller.ino
@@ -0,0 +1,1 @@
+void setup() {  pinMode(10, OUTPUT);  pinMode(11, OUTPUT);  digitalWrite(10, HIGH);  digitalWrite(11, LOW);  delay(5000);}void loop() {  digitalWrite(10, LOW);  delay(20);  digitalWrite(10, HIGH);}
